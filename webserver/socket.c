6a6cb63003bd91dbb5012e9a91c5fdfd23f019c6
Antoine DUQUENNOY <antoine.duquennoy@etudiant.univ-lille1.fr>
2015-09-22 16:19:39 +0200

    signal

--- webserver/socket.c
+++ webserver/socket.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <signal.h>
 
 int socket_serveur;
 
@@ -38,4 +39,10 @@ int creer_serveur(int port){
 	}
 	return socket_serveur;
 
+}
+
+void initialiser_signaux(void){
+	if (signal(SIGPIPE , SIG_IGN) == SIG_ERR){
+		perror("signal");
+	}
 }
