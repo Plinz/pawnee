ae5a3cc9b94c6659569602ff8a06c415c63a7382
Plinz <antoine.duquennoy59@gmail.com>
2015-10-18 16:30:10 +0200

    Update socket.h

--- webserver/socket.h
+++ webserver/socket.h
@@ -12,7 +12,7 @@ typedef struct {
 int creer_serveur (int port);
 void initialiser_signaux(void);
 void traitement_signal(int sig);
-char * fgets_or_exit(char *buffer , int size , FILE *stream);
+char* fgets_or_exit(char *buffer , int size , FILE *stream);
 int parse_http_request(const char *request_line , http_request *request);
 void skip_headers(FILE *client);
 void send_status(FILE *client , int code , const char *reason_phrase);
