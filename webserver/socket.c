5777ca8ecceeaab022e01c2d23fb36da3beb2b38
Plinz <antoine.duquennoy59@gmail.com>
2015-10-18 16:29:36 +0200

    Update socket.c

--- webserver/socket.c
+++ webserver/socket.c
@@ -50,7 +50,7 @@ int creer_serveur(int port){
 
 }
 
-char *fgets_or_exit(char *buffer , int size , FILE *stream){
+char * fgets_or_exit(char *buffer , int size , FILE *stream){
 	char *buf;	
 	if((buf = fgets(buffer, size, stream)) == NULL) {
 		exit(1);
