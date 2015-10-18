e721622aaace4012d08434b53919cfa4a941d4d7
Plinz <antoine.duquennoy59@gmail.com>
2015-10-18 16:26:57 +0200

    Update socket.h

--- webserver/socket.h
+++ webserver/socket.h
@@ -2,6 +2,13 @@
 #define __SOCKET_H__
 #include <stdio.h>
 
+typedef struct {
+	enum	http_method method;
+	int	major_version;
+	int	minor_version;
+	char	*url;
+} http_request;
+
 int creer_serveur (int port);
 void initialiser_signaux(void);
 void traitement_signal(int sig);
@@ -10,5 +17,6 @@ int parse_http_request(const char *request_line , http_request *request);
 void skip_headers(FILE *client);
 void send_status(FILE *client , int code , const char *reason_phrase);
 void send_response(FILE *client , int code , const  char *reason_phrase , const  char *message_body);
+
 
 #endif
